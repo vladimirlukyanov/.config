1248cd21bbea77ebe399341a75531cb7edbc7b22
Vladimir Lukyanov <vladimir@lukyanov.net>
2024-01-13 20:40:56 +0000

    v0.1.7

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
     starship init fish | source
+    set -x http_proxy "http://localhost:8888"
+    set -x https_proxy $http_proxy
+    set -x ftp_proxy $http_proxy
+    set -x rsync_proxy $http_proxy
 end
