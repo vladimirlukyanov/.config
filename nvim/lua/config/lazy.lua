b74e8811084fa898d0dfb613b4ce2e842bfc5913
Vladimir Lukyanov <vladimir@lukyanov.net>
2025-08-05 23:28:38 +0000

    v0.3.18
    - neovim config update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,7 +29,9 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "dracula" } },
+  install = { colorscheme = { "darcula-dark" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+vim.cmd.colorscheme("darcula-dark")
